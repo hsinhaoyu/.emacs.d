a35401711785f7b36a1b884b5e5f1f66c1d15643
Hsin-Hao Yu <hsinhaohh.yu@gmail.com>
2022-01-27 23:40:53 +1100

    Upgraded to Emacs 27.2. Theme didnt load. Added (load-theme...)

--- custom-file.el
+++ custom-file.el
@@ -5,12 +5,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(org-agenda-files (quote ("~/.deft/learn_org.org")))
+   '("3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(org-agenda-files '("~/.deft/learn_org.org"))
  '(package-selected-packages
-   (quote
-    (which-key projectile org-tree-slide visual-fill-column counsel doom-modeline ivy ess zenburn-theme company-jedi anaconda-mode one-themes nofrils-acme-theme smartparens smartparens-mode smartparen-mode company-org-roam olivetti org-roam go-mode exec-path-from-shell dockerfile-mode org-mode htmlize org-bullets org-journal org-super-agenda deft markdown-mode taskpaper-mode elpy ido-completing-read+ magit use-package company spacemacs-theme))))
+   '(zenburn which-key projectile org-tree-slide visual-fill-column counsel doom-modeline ivy ess zenburn-theme company-jedi anaconda-mode one-themes nofrils-acme-theme smartparens smartparens-mode smartparen-mode company-org-roam olivetti org-roam go-mode exec-path-from-shell dockerfile-mode org-mode htmlize org-bullets org-journal org-super-agenda deft markdown-mode taskpaper-mode elpy ido-completing-read+ magit use-package company spacemacs-theme)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
